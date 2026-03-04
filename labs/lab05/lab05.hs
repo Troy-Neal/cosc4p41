2955340c1dfda3a3141ffd8d60abbdbd2fb91cf2
Troy Neal <troyarthurdavid@gmail.com>
2026-03-04 13:01:00 -0500

    End Of Lab05
    This is how far we got into lab05.

--- labs/lab05/lab05.hs
+++ labs/lab05/lab05.hs
@@ -5,42 +5,78 @@ import qualified Text.ParserCombinators.Parsec.Token as Token
 import System.IO
 
 data Expr = Lit Integer | Var Var | Op Ops Expr Expr
-data Ops = Add | Sub | Mul | Div | Mod
+data Ops = Add | Sub | Mul | Div | Mod 
 type Var = String 
 
-prec :: Ops -> Int
 
+prec :: Ops -> Int
+prec Add = 3
+prec Sub = 3
+prec Mul = 6
+prec Div = 6
+prec Mod = 6
 
 -- Make Ops printable 
 
+instance Show (Ops) where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+  show Mod = "%"
+
 
 -- Make Expr printable 
+instance Show (Expr) where
+  showsPrec _ (Lit n) = shows n
+  showsPrec _ (Var v) = shows v
+  showsPrec p (Op operator le re) = showParen(p > pres) (showsPrec pres le).shows operator.(showsPrec pres re)
+    where pres = prec operator
 
 type Store = Var -> Integer
 
 initial :: Store 
 initial = const 0
 
-value   :: Store -> Var -> Integer 
+value :: Store -> Var -> Integer
+value = ($)
 
+update  :: Store -> Var -> Integer -> Store
+update st var val = \w -> if w == var then val else st w
 
-update  :: Store -> Var -> Integer -> Store 
-
+secondStore =  (update (update (update initial "w" 8) "y" 10)"z" 16)
 
 semOps :: Ops -> (Integer -> Integer -> Integer)
-
-
+semOps Add = (+)
+semOps Sub = (-)
+semOps Div = (div)
+semOps Mod = (mod)
+semOps Mul = (*)
 
 eval :: Expr -> Store -> Integer
-
-
+eval (Lit n) st = n
+eval (Var v) st = st v
+eval (Op operator le re) st = semOps operator (eval le st) (eval re st)
 
 data Command = Eval Expr | Assign Var Expr | Null deriving Show
 
-command :: Command -> Store -> (Integer,Store) 
+command :: Command -> Store -> (Integer,Store)
+command Null st = (0, st)
+command (Eval expr) st = (eval expr st, st)
+command (Assign var expr) st = (result, newStore)
+  where result = eval expr st
+        newStore = update st var result
 
-         
 languageDef :: LanguageDef ()
+languageDef = emptyDef {
+                Token.identStart = letter,
+                Token.identLetter = alphaNum,
+                Token.reservedNames = ["Store", "Null", "Eval", "."],
+                Token.reservedOpNames = ["+", "-", "*", "/", "%", ":="]
+              }
+{-
+     
+
 
 
 lexer :: Token.TokenParser ()
@@ -89,4 +125,4 @@ calcSteps :: Store -> IO ()
                   
 main :: IO ()
                 
-
+-}
